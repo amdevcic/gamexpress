ee8c7f18f888b81eba009873bf85b4d99bd993b6
Nikola Glavak <nikola.glavak@gmail.com>
2024-06-08 19:40:03 +0200

    add instructions button

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -3,6 +3,11 @@ using System;
 
 public partial class MainMenu : Control
 {
+
+	[Export] public Container mainContainer;
+	[Export] public Container instructionsContainer;
+	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -18,4 +23,17 @@ public partial class MainMenu : Control
 		GD.Print("play pressed");
 		GetTree().ChangeSceneToFile("res://Scenes/game.tscn");
 	}
+
+	public void _on_instructions_button_pressed()
+	{
+		mainContainer.Visible = false;
+		instructionsContainer.Visible = true; 
+	}
+
+	public void _on_back_button_pressed()
+	{
+		mainContainer.Visible = true;
+		instructionsContainer.Visible = false;
+	}
+
 }
