b672db63e2291e4fc48b099041a33b97441dbaf9
Nikola Glavak <nikola.glavak@gmail.com>
2024-06-08 23:00:47 +0200

    make populating random

--- Scripts/PotionShelf.cs
+++ Scripts/PotionShelf.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+
 
 public partial class PotionShelf : Node
 {
@@ -31,26 +33,50 @@ public partial class PotionShelf : Node
     }
 
     public void Populate() {
-    int count = Math.Abs((int)GD.Randi()) % (width * height - 3) + 3;
-    int populated = 0;
+        int count = Math.Abs((int)GD.Randi()) % (width * height - 3) + 3;
+        int populated = 0;
 
-    GD.Print($"Populating {count} elements.");
+        GD.Print($"Populating {count} elements.");
 
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+        // Generate a list of all grid positions
+        List<(int, int)> gridPositions = new List<(int, int)>();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                gridPositions.Add((i, j));
+            }
+        }
+
+        // Shuffle the list of grid positions
+        Shuffle(gridPositions);
+
+        // Populate the shuffled grid positions
+        foreach (var pos in gridPositions) {
             if (populated >= count) break;
 
-            Slot s = slots[i, j];
+            Slot s = slots[pos.Item1, pos.Item2];
             SpawnItem(s);
             populated++;
         }
-        if (populated >= count) break;
-    }
-    Slot h = slots[0, 0];
-    h.SetElement(defaultElement);
 
-    GD.Print($"Populated {populated} elements.");
-}
+        Slot h = slots[0, 0];
+        h.SetElement(defaultElement);
+
+        GD.Print($"Populated {populated} elements.");
+    }
+
+    // Method to shuffle a list using Fisher-Yates algorithm
+    private void Shuffle<T>(List<T> list) {
+        Random rng = new Random();
+        int n = list.Count;
+        while (n > 1) {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
 
     public void ClearPotions() {
         for (int i = 0; i < height; i++) {
