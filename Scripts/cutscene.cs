12b6eb9ae571af34f1d0b0f896495e719921654e
Nikola Glavak <nikola.glavak@gmail.com>
2024-06-09 04:23:19 +0200

    add basic cutscene

---
+++ Scripts/cutscene.cs
@@ -0,0 +1,78 @@
+using Godot;
+using System;
+using System.Collections;
+
+public partial class cutscene : Node2D
+{
+	private Timer timer;
+	private HBoxContainer textbox;
+	private TextureRect portrait;
+	private Label text;
+	private int currentLine = 0;
+
+	[Export] public Texture2D portrait1;
+	[Export] public Texture2D portrait2;
+
+	
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		timer = GetNode<Timer>("Timer");
+		textbox = GetNode<HBoxContainer>("CanvasLayer/HBoxContainer");
+		portrait = GetNode<TextureRect>("CanvasLayer/HBoxContainer/NinePatchRect/MarginContainer/TextureRect");
+		text = GetNode<Label>("CanvasLayer/HBoxContainer/NinePatchRect2/MarginContainer/Label");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override async void _Process(double delta)
+	{
+		if(Input.IsAnythingPressed())
+		{
+			AdvanceText(currentLine);
+			currentLine += 1;
+			await ToSignal(GetTree().CreateTimer(0.2f), SceneTreeTimer.SignalName.Timeout);
+		}
+	}
+
+	private void AdvanceText(int line)
+	{
+		switch(line)
+		{
+			case 1:
+				text.Text ="Apprentice. I'm going out.";
+				break;
+			case 2:
+				text.Text = "Don't touch anything.";
+				break;
+			case 3:
+				portrait.Texture = portrait1;
+				text.Text = "Yes, grand master alchemist!";
+				break;
+			case 4:
+				textbox.Visible = false;
+				break;
+			case 5:
+				textbox.Visible = true;
+				text.Text = "I'm sure nothing will happen if I just...";
+				break;
+			case 6:
+				textbox.Visible = false;
+				break;
+			case 7:
+				textbox.Visible = true;
+				text.Text = "Oh no.";
+				break;
+			case 8:
+				textbox.Visible = true;
+				text.Text = "Um... I'm sure I can stabilize it somehow!";
+				break;
+			case 9:
+				GetTree().ChangeSceneToFile("res://Scenes/game.tscn");
+				break;
+			default:
+				break;
+		}
+	}
+
+}
